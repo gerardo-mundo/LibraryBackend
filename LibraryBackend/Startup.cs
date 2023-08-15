1a49c16d7d342e1600a5ab11d796235ca02cbc9b
Gerardo Mundo <ger.mundo66@gmail.com>
2023-08-15 14:19:02 -0600

    Initial project configuration

---
+++ LibraryBackend/Startup.cs
@@ -0,0 +1,66 @@
+﻿using LibraryBackend.context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace LibraryBackend
+{
+    public class Startup
+    {
+        public Startup(ConfigurationManager configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public OpenApiLicense MIT { get; private set; } = null!;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = 
+            ReferenceHandler.IgnoreCycles);
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection"));
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+            services.AddSwaggerGen(c => 
+            c.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Title = "Library WebApi", Description = "WebApi for library management", License = MIT, Version = "v1", 
+                Contact = new OpenApiContact() { Name = "Gerardo Mundo", Email = "gerardo.perez@udgvirtual.udg.mx" }
+            }));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddDataProtection();
+
+            services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyMethod()
+            .WithOrigins("")));
+        }
+
+       public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
---
+++ LibraryBackend/context/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryBackend.context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+       // public DbSet<Book> Books { get; set; }
+
+    }
+}
